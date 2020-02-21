c6f38e3efddad82b5492ab74499e844ac476094e
zhiwenhe <18676797056@163.com>
2020-02-21 15:13:14 +0800

    ol draw.

--- src/pages/Map/Map5.js
+++ src/pages/Map/Map5.js
@@ -2,22 +2,22 @@ import React, { Component } from 'react';
 
 import Map from 'ol/Map';
 import View from 'ol/View';
-import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer'
-import { OSM as OSMSource, Vector as VectorSource } from 'ol/source'
-import Draw from 'ol/interaction/Draw'
+import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer';
+import { OSM as OSMSource, Vector as VectorSource } from 'ol/source';
+import Draw from 'ol/interaction/Draw';
 
 class Map5 extends Component {
   constructor(props) {
-    super(props)
-    this.mapRef = React.createRef()
+    super(props);
+    this.mapRef = React.createRef();
     this.state = {
-      type: 'Point'
-    }
-    this.handleTypeChange = this.handleTypeChange.bind(this)
+      type: 'Point',
+    };
+    this.handleTypeChange = this.handleTypeChange.bind(this);
   }
 
   render() {
-    const { type } = this.state
+    const { type } = this.state;
     return (
       <div>
         <div ref={this.mapRef} style={{ width: '100%', height: '400px' }} />
@@ -35,14 +35,14 @@ class Map5 extends Component {
 
   componentDidMount() {
     const raster = new TileLayer({
-      source: new OSMSource()
-    })
+      source: new OSMSource(),
+    });
 
-    this.vectorSource = new VectorSource()
+    this.vectorSource = new VectorSource();
 
     const vector = new VectorLayer({
-      source: this.vectorSource
-    })
+      source: this.vectorSource,
+    });
 
     this.map = new Map({
       target: this.mapRef.current,
@@ -53,7 +53,7 @@ class Map5 extends Component {
       layers: [raster, vector],
     });
 
-    this.addInteraction()
+    this.addInteraction(this.state.type);
   }
 
   componentWillUnmount() {
@@ -61,25 +61,21 @@ class Map5 extends Component {
   }
 
   handleTypeChange(event) {
-    const type = event.target.value
+    const type = event.target.value;
     this.setState({
-      type
-    })
+      type,
+    });
+    this.map.removeInteraction(this.draw);
+    this.addInteraction(type);
   }
 
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    this.addInteraction()
-  }
-
-  addInteraction() {
-    const { type } = this.state
-    this.map.removeInteraction(this.draw)
-    if (type == 'None') return
+  addInteraction(type) {
+    if (type == 'None') return;
     this.draw = new Draw({
       type,
-      source: this.vectorSource
-    })
-    this.map.addInteraction(this.draw)
+      source: this.vectorSource,
+    });
+    this.map.addInteraction(this.draw);
   }
 }
 
