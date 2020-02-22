f0b0f29c2794ccd704ea01503ac3f0ff7668e2cf
zhiwenhe <18676797056@163.com>
2020-02-22 14:20:26 +0800

    ol MousePosition.

--- src/pages/Map/Map7.js
+++ src/pages/Map/Map7.js
@@ -6,14 +6,20 @@ import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer';
 import { OSM, Vector as VectorSource } from 'ol/source';
 import { fromLonLat, toLonLat } from 'ol/proj';
 import { Draw } from 'ol/interaction';
-import { toStringXY } from 'ol/coordinate';
+import { toStringXY, createStringXY } from 'ol/coordinate';
+import { defaults as defaultControls } from 'ol/control';
+import MousePosition from 'ol/control/MousePosition';
 
 class Map7 extends Component {
   constructor(props) {
     super(props);
+
     this.mapRef = React.createRef();
-    this.coordsRef = React.createRef();
+    this.posRef = React.createRef();
+    this.pos1Ref = React.createRef();
+
     this.handleAddFeature = this.handleAddFeature.bind(this);
+
     this.state = {};
   }
 
@@ -23,6 +29,18 @@ class Map7 extends Component {
 
     const vectorSource = new VectorSource();
     vectorSource.on('addfeature', this.handleAddFeature);
+
+    const mousePosition = new MousePosition({
+      coordinateFormat: createStringXY(4),
+      target: this.posRef.current,
+      className: 'custom-mouse-position',
+    });
+    const mousePosition1 = new MousePosition({
+      coordinateFormat: createStringXY(4),
+      projection: 'EPSG:4326',
+      target: this.pos1Ref.current,
+      className: 'custom-mouse-position',
+    });
 
     this.map = new Map({
       target: this.mapRef.current,
@@ -38,6 +56,7 @@ class Map7 extends Component {
           source: vectorSource,
         }),
       ],
+      controls: defaultControls().extend([mousePosition, mousePosition1]),
     });
 
     this.map.addInteraction(
@@ -63,9 +82,17 @@ class Map7 extends Component {
     return (
       <div>
         <div ref={this.mapRef} style={{ width: '100%', height: '400px' }} />
-        <span>EPSG:3857 {coords ? toStringXY(coords) : ''}</span>
+        EPSG:3857
+        <div ref={this.posRef} />
         <br />
-        <span>LonLat {coords ? toStringXY(toLonLat(coords)) : ''}</span>
+        EPSG:4326
+        <div ref={this.pos1Ref} />
+        <br />
+        EPSG:3857
+        <div>{coords ? toStringXY(coords, 4) : ''}</div>
+        <br />
+        EPSG:4326
+        <div>{coords ? toStringXY(toLonLat(coords), 4) : ''}</div>
       </div>
     );
   }
