ea09a37b8ef863594800a0c1fb6a4140928acc37
zhiwenhe <18676797056@163.com>
2020-02-23 14:45:43 +0800

    ol Map3.

--- src/pages/Map/Map3.js
+++ src/pages/Map/Map3.js
@@ -2,58 +2,59 @@ import React, { Component } from 'react';
 
 import Map from 'ol/Map';
 import View from 'ol/View';
-import { Tile as TileLayer, Heatmap as HeatmapLayer } from 'ol/layer'
-import Stamen from 'ol/source/Stamen'
-import VectorSource from 'ol/source/Vector'
+import { Tile as TileLayer, Vector as VectorLayer, Heatmap as HeatmapLayer } from 'ol/layer';
+import Stamen from 'ol/source/Stamen';
+import VectorSource from 'ol/source/Vector';
 import KML from 'ol/format/KML';
+
+import styles from './Map3.less';
 
 class Map3 extends Component {
   constructor(props) {
-    super(props)
-    this.mapRef = React.createRef()
+    super(props);
+    this.mapRef = React.createRef();
     this.state = {
       radius: 5,
-      blur: 15
-    }
-  }
-
-  render() {
-    const { radius, blur } = this.state
-    return (
-      <div>
-        <div ref={this.mapRef} style={{ width: '100%', height: '400px' }} />
-        <label>radius size</label><br />
-        <input ref={this.radiusRef} type="range" min="1" max="50" step="1" value={radius} onChange={this.handleRadiusChange.bind(this)} /><br />
-        <label>blur size</label><br />
-        <input ref={this.blurRef} type="range" min="1" max="50" step="1" value={blur} onChange={this.handleBlurChange.bind(this)} />
-      </div>
-    );
+      blur: 15,
+      showHeatmap: false,
+    };
+    this.toggleHeatmap = this.toggleHeatmap.bind(this);
   }
 
   componentDidMount() {
+    const { radius, blur, showHeatmap } = this.state;
+
     const raster = new TileLayer({
       source: new Stamen({
-        layer: 'toner'
-      })
-    })
+        layer: 'toner',
+      }),
+    });
 
-    const { radius, blur } = this.state
-    this.vector = new HeatmapLayer({
+    const location = new VectorLayer({
       source: new VectorSource({
         url: './2012_Earthquakes_Mag5.kml',
         format: new KML({
-          extractStyles: false
-        })
+          extractStyles: false,
+        }),
+      }),
+    });
+
+    this.heatmap = new HeatmapLayer({
+      source: new VectorSource({
+        url: './2012_Earthquakes_Mag5.kml',
+        format: new KML({
+          extractStyles: false,
+        }),
       }),
       radius: parseInt(radius, 10),
       blur: parseInt(blur, 10),
-      weight: function(feature) {
-        const name = feature.get('name')
-        // console.log(name, name.substr(2))
-        const magnitude = parseFloat(name.substr(2))
-        return magnitude - 5
-      }
-    })
+      weight(feature) {
+        const name = feature.get('name');
+        const magnitude = parseFloat(name.substr(2));
+        return magnitude - 5;
+      },
+      visible: showHeatmap,
+    });
 
     this.map = new Map({
       target: this.mapRef.current,
@@ -61,7 +62,7 @@ class Map3 extends Component {
         center: [0, 0],
         zoom: 2,
       }),
-      layers: [raster, this.vector],
+      layers: [raster, location, this.heatmap],
     });
   }
 
@@ -70,19 +71,66 @@ class Map3 extends Component {
   }
 
   handleRadiusChange(event) {
-    const radius = event.target.value
-    this.vector.setRadius(parseInt(radius, 10))
+    const radius = event.target.value;
+    this.heatmap.setRadius(parseInt(radius, 10));
     this.setState({
-      radius
-    })
+      radius,
+    });
   }
 
   handleBlurChange(event) {
-    const blur = event.target.value
-    this.vector.setBlur(parseInt(blur, 10))
+    const blur = event.target.value;
+    this.heatmap.setBlur(parseInt(blur, 10));
     this.setState({
-      blur
-    })
+      blur,
+    });
+  }
+
+  toggleHeatmap() {
+    const { showHeatmap } = this.state;
+    this.setState({
+      showHeatmap: !showHeatmap,
+    });
+    this.heatmap.setVisible(!showHeatmap);
+  }
+
+  render() {
+    const { radius, blur, showHeatmap } = this.state;
+
+    return (
+      <div>
+        <div ref={this.mapRef} className={styles.map} />
+        <input type="checkbox" onChange={this.toggleHeatmap} checked={showHeatmap} /> 热力图
+        <br />
+        <label htmlFor="radius">
+          radius size
+          <br />
+          <input
+            id="radius"
+            type="range"
+            min="1"
+            max="50"
+            step="1"
+            value={radius}
+            onChange={this.handleRadiusChange.bind(this)}
+          />
+        </label>
+        <br />
+        <label htmlFor="blur">
+          blur size
+          <br />
+          <input
+            id="blur"
+            type="range"
+            min="1"
+            max="50"
+            step="1"
+            value={blur}
+            onChange={this.handleBlurChange.bind(this)}
+          />
+        </label>
+      </div>
+    );
   }
 }
 
