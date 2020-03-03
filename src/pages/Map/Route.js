ffdd9994da9c11e30b547080284d7ab0f22a97f3
zhiwenhe <18676797056@163.com>
2020-03-03 15:25:53 +0800

    route.

--- src/pages/Map/Route.js
+++ src/pages/Map/Route.js
@@ -4,6 +4,17 @@ import { connect } from 'dva';
 
 import { Select, DatePicker, Button, Cascader } from 'antd';
 
+import { isEqual } from 'lodash';
+
+import Map from 'ol/Map';
+import View from 'ol/View';
+import { Image as ImageLayer, Vector as VectorLayer } from 'ol/layer';
+import { Vector as VectorSource, ImageStatic } from 'ol/source';
+import Projection from 'ol/proj/Projection';
+import { getCenter } from 'ol/extent';
+
+import styles from './Route.less';
+
 @connect(({ monitor, route }) => ({
   monitor,
   route,
@@ -15,6 +26,79 @@ class Route extends Component {
     this.changeDatetime = this.changeDatetime.bind(this);
     this.handleSearch = this.handleSearch.bind(this);
     this.changePlace = this.changePlace.bind(this);
+  }
+
+  componentDidMount() {
+    const mapLayer = new ImageLayer({
+      visible: false,
+    });
+
+    this.routeSource = new VectorSource();
+
+    const routeLayer = new VectorLayer({
+      source: this.routeSource,
+    });
+
+    const layers = [mapLayer, routeLayer];
+
+    this.map = new Map({
+      target: 'map',
+      layers,
+    });
+  }
+
+  componentDidUpdate() {
+    const {
+      route: { place, routes },
+    } = this.props;
+    const fr = routes.filter(r => isEqual(r.place, place));
+    const [route] = fr;
+
+    if (!route) {
+      this.url = undefined;
+      this.map
+        .getLayers()
+        .item(0)
+        .setVisible(false);
+      this.map
+        .getLayers()
+        .item(1)
+        .getSource()
+        .clear();
+      return;
+    }
+
+    const {
+      map: { url, extent },
+    } = route;
+
+    if (this.url === url) return;
+    this.url = url;
+
+    const projection = new Projection({
+      code: 'hzw',
+      extent,
+    });
+
+    const view = new View({
+      center: getCenter(extent),
+      zoom: 2,
+      projection,
+    });
+    this.map.setView(view);
+
+    const mapLayer = new ImageLayer({
+      source: new ImageStatic({
+        url,
+        imageExtent: extent,
+      }),
+    });
+    this.map.getLayers().removeAt(0);
+    this.map.getLayers().insertAt(0, mapLayer);
+  }
+
+  componentWillUnmount() {
+    this.map.setTarget(undefined);
   }
 
   changePerson(person) {
@@ -94,6 +178,7 @@ class Route extends Component {
           onChange={this.changePlace}
           placeholder="请选择地址"
         />
+        <div id="map" className={styles.map} />
       </div>
     );
   }
