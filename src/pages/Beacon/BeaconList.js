dcb2e6513eb31a0e02bed3003a5e0d4494e8e5ed
chongmeizhao <chongmeizhao@kaifa.cn>
2020-03-09 15:26:09 +0800

    search list

--- src/pages/Beacon/BeaconList.js
+++ src/pages/Beacon/BeaconList.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'dva';
-import { Table,Button,Card , Modal,Form,Input,Popconfirm,Dropdown,Menu} from 'antd';
+import { Form, Row, Col, Card, Button, Table, Modal,Input,Popconfirm,Dropdown,Menu} from 'antd';
 
 import PageHeaderWrapper from '@/components/PageHeaderWrapper';
 import styles from './BeaconList.less';
 
-import { queryBeacons,deleteBeacons } from '@/services/sh';
+import { queryBeacons, deleteBeacon } from '@/services/sh';
 
 function getColumns(operations) {
   const columns = [
@@ -42,101 +42,147 @@ function getColumns(operations) {
 
   return columns
 }
-
+@Form.create()
 class BeaconList extends Component {
   state = {
-    page: 1,
-    rows: 10,
-    beacons: [],
     loading: false,
+    pagination: {
+      page: 1,
+      pageSize: 10,
+    },
+    params: {},
+    beacons: [],
     total: 0,
-    mac: '',
-    name: '',
   }
 
-  handleSearch = () => {
-    const { rows, mac, name } = this.state
+  searchBeacons = (pagination, params) => {
     this.setState({
       loading: true,
-      page: 1,
     })
-    queryBeacons({ page:1 , rows, mac, name }).then(response => {
-      // setTimeout(() => {
+
+    queryBeacons(pagination, params).then(response => {
+      setTimeout(() => {
         this.setState({
-          beacons: response.result.rows,
           loading: false,
+          beacons: response.result.rows,
           total: parseInt(response.result.total, 10),
+          pagination,
         })
-      // }, 3000)
+      }, 3000)
     })
   }
+
+  handleSearch = e => {
+    e.preventDefault();
+
+    const { form } = this.props;
+
+    form.validateFields((err, fieldsValue) => {
+      if (err) return;
+
+      const params = fieldsValue
+
+      this.setState({
+        params,
+      })
+
+      const pagination = { ...this.state.pagination }
+      pagination.page = 1
+      this.searchBeacons(pagination, params)
+    });
+  }
+
+  handleFormReset = () => {
+    const { form } = this.props;
+    
+    form.resetFields();
+    this.setState({
+      params: {},
+    })
+
+    const pagination = { ...this.state.pagination }
+    pagination.page = 1
+    this.searchBeacons(pagination, {})
+  };
 
   handlePaginationChange = (page, pageSize) => {
-    this.setState({
-      loading: true,
-      page,
-      rows: pageSize,
-    })
-    queryBeacons({ page, pageSize }).then(response => {
-      // setTimeout(() => {
-        this.setState({
-          beacons: response.result.rows,
-          loading: false,
-          total: parseInt(response.result.total, 10),
-        })
-      // }, 3000)
-    })
+    const { params } = this.state
+    const pagination = { page, pageSize }
+    this.searchBeacons(pagination, params)
   }
 
-  handleMacChange = (event) => {
-    this.setState({
-      mac: event.target.value,
-    })
-  }
-
-  handleNameChange = (event) => {
-    this.setState({
-      name: event.target.value,
-    })
-  }
-
-  handleDelete = (id) => {
-    const { page, rows, mac, name } = this.state
+  handleDelete = id => {
+    const { pagination, params } = this.state
 
     this.setState({
       loading: true,
     })
 
-    deleteBeacons({ id }).then(response => {   
-      queryBeacons({ page , rows, mac, name }).then(response => {
-        // setTimeout(() => {
-          this.setState({
-            beacons: response.result.rows,
-            loading: false,
-            total: parseInt(response.result.total, 10),
-          })
-        // }, 3000)
-      })
+    deleteBeacon({ id }).then(response => {   
+      this.searchBeacons(pagination, params)
     })
+  }
+
+  renderForm = loading => {
+    const { form: { getFieldDecorator } } = this.props
+    
+    return (
+      <Form onSubmit={this.handleSearch} layout="inline">
+        <Row gutter={{ lg: 24, xl: 48 }}>
+          <Col md={8}>
+            <Form.Item label="Mac">
+              {getFieldDecorator('mac')(<Input placeholder="请输入" />)}
+            </Form.Item>      
+          </Col>
+          <Col md={8}>
+            <Form.Item label="Name">
+              {getFieldDecorator('name')(<Input placeholder="请输入" />)}
+            </Form.Item>
+          </Col>
+          <Col md={8}>
+            <Form.Item label="Number">
+              {getFieldDecorator('number')(<Input placeholder="请输入" />)}
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row gutter={{ lg: 24, xl: 48 }}>
+          <Col md={8}>
+            <Form.Item label="Type">
+              {getFieldDecorator('type')(<Input placeholder="请输入" />)}
+            </Form.Item>      
+          </Col>
+        </Row>
+        <div style={{ overflow: 'hidden' }}>
+          <div style={{ float: 'right', marginBottom: 24 }}>
+            <Button type="primary" htmlType="submit" loading={loading}>
+              查询
+            </Button>
+            <Button style={{ marginLeft: 8 }} loading={loading}  onClick={this.handleFormReset}>
+              重置
+            </Button>
+          </div>
+        </div>
+      </Form>
+    )
   }
 
   render() {
-    const { page, rows, beacons, loading, total, mac, name } = this.state
+    const { loading, pagination: { page, pageSize }, beacons, total } = this.state
 
     return (
       <PageHeaderWrapper>
-        <div className={styles.standardList}>
-          <Input addonBefore="MAC" placeholder="请输入" value={mac} onChange={this.handleMacChange}  />
-          <Input addonBefore="姓名" placeholder="请输入" value={name} onChange={this.handleNameChange}  />
-          <Button onClick={this.handleSearch} loading={loading}>查询</Button>
+        <Card>
+          <div className={styles.tableListForm}>
+            {this.renderForm(loading)}
+          </div>
           <Table 
             dataSource={beacons}
             columns={getColumns({delete: this.handleDelete})}
             rowKey="id"
             loading={loading}
-            pagination={{current: page, pageSize: rows, total, onChange: this.handlePaginationChange}}
+            pagination={{current: page, pageSize, total, onChange: this.handlePaginationChange}}
           />
-        </div>
+        </Card>
      </PageHeaderWrapper>
     );
   }
--- src/services/sh.js
+++ src/services/sh.js
@@ -2,15 +2,19 @@ import request from '@/utils/request';
 
 import { HTTP_API_ROOT } from './constants'
 
-export async function queryBeacons(params) {
+export async function queryBeacons(pagination, params) {
+    const body = { ...pagination, ...params }
+    body.rows = body.pageSize
+    delete body.pageSize
+    
     return request(`${HTTP_API_ROOT}/config/beacon_list`, {
         method: 'POST',
-        body: params,
+        body,
         credentials: 'omit',
     });
 }
   
-export async function deleteBeacons(params) {
+export async function deleteBeacon(params) {
     return request(`${HTTP_API_ROOT}/config/beacon_delete`, {
         method: 'POST',
         body: params,
